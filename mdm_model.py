12ae0e8f7d1aa4e48b57c6cce46c6806c04e6792
Joker Yough <of19950402@gmail.com>
2018-12-24 19:43:28 +0800

    1st version

--- mdm_model.py
+++ mdm_model.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import utils
 
-extract_patches_module = tf.load_op_library('extract_patches_op/extract_patches.so')
-extract_patches = extract_patches_module.extract_patches
-tf.NotDifferentiable('ExtractPatches')
+# extract_patches_module = tf.load_op_library('extract_patches_op/extract_patches.so')
+# extract_patches = extract_patches_module.extract_patches
+# tf.NotDifferentiable('ExtractPatches')
 
 
 def align_reference_shape(reference_shape, reference_shape_bb, im, bb):
@@ -16,6 +16,94 @@ def align_reference_shape(reference_shape, reference_shape_bb, im, bb):
     return tf.image.resize_bilinear(tf.expand_dims(im, 0), new_size)[0, :, :, :], align_mean_shape / ratio, ratio
 
 
+def _conv2d(
+        inputs,
+        filters,
+        kernel_size,
+        strides=(1, 1),
+        activation=None,
+        use_bias=True,
+        use_bn=False,
+        training=False,
+        name='Convolution'
+):
+    with tf.variable_scope(name, values=[inputs]):
+        inputs = tf.layers.conv2d(inputs, filters, kernel_size, strides, padding='same', use_bias=use_bias)
+        if use_bn:
+            inputs = tf.layers.batch_normalization(inputs, training=training)
+        if activation is not None:
+            inputs = activation(inputs)
+    return inputs
+
+
+def _conv2d_dw(
+        inputs,
+        filters,
+        kernel_size,
+        strides=(1, 1),
+        activation=None,
+        use_bias=True,
+        use_bn=False,
+        training=False,
+        name='ConvolutionDepthWise'
+):
+    with tf.variable_scope(name, values=[inputs]):
+        layer = tf.keras.layers.DepthwiseConv2D(
+            kernel_size, strides,
+            padding='same', use_bias=use_bias)
+        inputs = layer.apply(inputs)
+        if use_bn:
+            inputs = tf.layers.batch_normalization(inputs, training=training)
+        inputs = tf.layers.conv2d(inputs, filters, [1, 1], padding='same', use_bias=use_bias)
+        if use_bn:
+            inputs = tf.layers.batch_normalization(inputs, training=training)
+        if activation is not None:
+            inputs = activation(inputs)
+    return inputs
+
+
+def _shuffle_block(
+        inputs,
+        in_filters,
+        out_filters,
+        kernel_size,
+        strides,
+        depth,
+        training=False,
+        name='ShuffleBlock'
+):
+    with tf.variable_scope(name, values=[inputs]):
+        with tf.variable_scope('Unit0'):
+            left = _conv2d(
+                inputs, in_filters, [1, 1],
+                activation=tf.nn.relu, use_bias=False, use_bn=True, training=training, name='Convolution1x1'
+            )
+            left = _conv2d_dw(
+                left, out_filters // 2, kernel_size, strides,
+                activation=tf.nn.relu, use_bias=False, use_bn=True, training=training, name='Convolution3x3DepthWise'
+            )
+            right = _conv2d_dw(
+                inputs, out_filters // 2, kernel_size, strides,
+                activation=tf.nn.relu, use_bias=False, use_bn=True, training=training, name='Bypass'
+            )
+        for i in range(1, depth):
+            with tf.variable_scope('Unit{}'.format(i)):
+                with tf.name_scope('ChannelShuffle'):
+                    ll, lr = tf.split(left, [out_filters // 4, out_filters // 4], -1)
+                    rl, rr = tf.split(right, [out_filters // 4, out_filters // 4], -1)
+                    left = tf.concat([ll, rr], -1)
+                    right = tf.concat([rl, lr], -1)
+                left = _conv2d(
+                    left, out_filters // 2, [1, 1],
+                    activation=tf.nn.relu, use_bias=False, use_bn=True, training=training, name='Convolution1x1'
+                )
+                left = _conv2d_dw(
+                    left, out_filters // 2, kernel_size, [1, 1],
+                    activation=tf.nn.relu, use_bias=False, use_bn=True, training=training, name='Convolution3x3DepthWise'
+                )
+        return tf.concat([left, right], -1)
+
+
 class MDMModel:
     def __init__(
             self, images, shapes, inits,
@@ -25,108 +113,49 @@ class MDMModel:
         self.in_images = images
         self.in_shapes = shapes
         self.in_init_shapes = inits
+        self.batch_size = batch_size
         self.num_iterations = num_iterations
         self.num_patches = num_patches
         self.patch_shape = patch_shape
         self.num_channels = num_channels
         self.is_training = is_training
 
-        self.batch_size = batch_size
-        self.hidden_size = 512
-        self.dxs = []
-        self.patches = []
-        self.cnn = []
-        self.rnn = []
-
-        with tf.name_scope('Network', values=[self.in_init_shapes]):
-            self.rnn_hidden = tf.zeros((self.batch_size, self.hidden_size), name='init_state')
-            self.dx = tf.zeros((self.batch_size, self.num_patches, 2), name='init_dx')
-            for step in range(self.num_iterations):
-                with tf.name_scope('ExtractPatches{}'.format(step), values=[self.in_images, self.dx]):
-                    with tf.device('/cpu:0'):
-                        patches = extract_patches(
-                            self.in_images,
-                            tf.constant(self.patch_shape),
-                            self.in_init_shapes + self.dx,
-                            name='patch'
-                        )
-                    # self.visualize_patches(step, patches)
-                    self.patches.append(patches)
-
-                with tf.variable_scope('cnn', reuse=tf.AUTO_REUSE, auxiliary_name_scope=False):
-                    rnn_in, net = self.conv_model(patches, step)
-                self.cnn.append(net)
-
-                with tf.variable_scope('rnn', reuse=tf.AUTO_REUSE, auxiliary_name_scope=False):
-                    with tf.name_scope('mdm_rnn{}'.format(step), values=[rnn_in, self.rnn_hidden]):
-                        self.rnn_hidden = tf.layers.dense(
-                            tf.concat([rnn_in, self.rnn_hidden], 1), self.hidden_size,
-                            activation=tf.tanh
-                        )
-                        prediction = tf.layers.dense(
-                            self.rnn_hidden, self.num_patches * 2,
-                            name='pred', activation=None
-                        )
-                        prediction = tf.reshape(prediction, (self.batch_size, self.num_patches, 2))
-                        self.dx += prediction
-                self.rnn.append(prediction)
-                self.dxs.append(self.dx)
-            self.prediction = tf.add(self.in_init_shapes, self.dx, name='prediction')
+        with tf.variable_scope('Network', values=[self.in_init_shapes]):
+            with tf.variable_scope('Initial'):
+                inputs = _conv2d(
+                    self.in_images, 64, [3, 3],
+                    activation=tf.nn.relu, use_bias=False, use_bn=True, training=self.is_training, name='Convolution'
+                )
+                inputs = tf.layers.max_pooling2d(inputs, [2, 2], [2, 2], name='MaxPooling')
+            inputs = _shuffle_block(
+                inputs, 64, 96, [3, 3], [2, 2], 4,
+                training=self.is_training, name='ShuffleBlock1'
+            )
+            inputs = _shuffle_block(
+                inputs, 96, 192, [3, 3], [2, 2], 8,
+                training=self.is_training, name='ShuffleBlock2'
+            )
+            inputs = _shuffle_block(
+                inputs, 192, 384, [3, 3], [2, 2], 4,
+                training=self.is_training, name='ShuffleBlock3'
+            )
+            with tf.variable_scope('Finalize'):
+                inputs = _conv2d(inputs, 1024, [1, 1], activation=tf.nn.relu, name='Convolution')
+                inputs = tf.layers.dropout(inputs, 0.8, training=self.is_training, name='Dropout')
+                inputs = tf.layers.average_pooling2d(inputs, [7, 7], [1, 1], name='AvgPooling')
+            with tf.variable_scope('Predict'):
+                inputs = _conv2d(inputs, 146, [1, 1], name='Convolution')
+                with tf.variable_scope('Temp'):
+                    inputs = tf.layers.flatten(inputs)
+                    inputs = tf.layers.dense(inputs, 146)  # Temp
+                inputs = tf.reshape(inputs, [-1, 73, 2])
+                self.prediction = inputs + self.in_init_shapes
             self.out_images, = tf.py_func(
                 utils.batch_draw_landmarks_discrete,
                 [self.in_images, self.in_shapes, self.prediction],
                 [tf.float32]
             )
             tf.summary.image('images', self.out_images, max_outputs=10)
-
-    def conv2d(self, inputs, name):
-        return tf.layers.conv2d(inputs, 32, [3, 3], activation=tf.nn.relu, name=name)
-
-    def conv2d_bn(self, inputs, name):
-        with tf.name_scope(name, values=[inputs]):
-            inputs = tf.layers.conv2d(inputs, 32, [3, 3], activation=None)
-            inputs = tf.layers.batch_normalization(inputs, training=self.is_training)
-            inputs = tf.nn.relu(inputs)
-        return inputs
-
-    def conv_model(self, inputs, step):
-        """
-        Construct the CNN
-        Args:
-            inputs: Tensor with shape [n, num_landmarks, patch_shape, patch_shape, 3]
-            step(int): RNN step
-        Returns:
-        """
-        net = {}
-        with tf.name_scope('mdm_conv{}'.format(step), values=[inputs]):
-            inputs = tf.reshape(
-                inputs,
-                (self.batch_size * self.num_patches, self.patch_shape[0], self.patch_shape[1], self.num_channels)
-            )
-            # Convolution 1
-            inputs = self.conv2d(inputs, name='conv_1')
-            self.visualize_cnn_mean(step, inputs, 'conv_1')
-            net['conv_1'] = inputs
-            inputs = tf.layers.max_pooling2d(inputs, [2, 2], [2, 2])
-            net['pool_1'] = inputs
-
-            # Convolution 2
-            inputs = self.conv2d(inputs, name='conv_2')
-            self.visualize_cnn_mean(step, inputs, 'conv_2')
-            net['conv_2'] = inputs
-            inputs = tf.layers.max_pooling2d(inputs, [2, 2], [2, 2])
-            net['pool_2'] = inputs
-
-            # Crop
-            crop_size = inputs.get_shape().as_list()[1:3]
-            cropped = utils.get_central_crop(net['conv_2'], box=crop_size)
-            net['conv_2_cropped'] = cropped
-            inputs = tf.concat([cropped, inputs], 3)
-
-            # Flatten
-            inputs = tf.reshape(inputs, (self.batch_size, -1))
-        net['concat'] = inputs
-        return inputs, net
 
     def visualize_patches(self, step, inputs):
         """
