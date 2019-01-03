572e7332c36b9fa25e27f7053d0d9ade49b6c9f6
Joker Yough <of19950402@gmail.com>
2019-01-03 13:58:21 +0800

    utils with test

---
+++ unit_test/utils_test.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import menpo
+import random
+import sys
+sys.path.append('..')
+
+from utils import *
+
+# =====Norm index test=====
+for _ in range(1000):
+    num = int(random.random() * 100)
+    if num == 73:
+        assert 31, 27 == norm_idx(num)
+    elif num == 68:
+        assert 36, 45 == norm_idx(num)
+    else:
+        try:
+            _, _ = norm_idx(num)
+            raise ValueError
+        except AssertionError as e:
+            pass
+
+
+# =====Mirror test=====
+test_image = menpo.io.import_image('../Dataset/300W/Outdoor/Images/outdoor_004.png')
+assert isinstance(test_image, menpo.image.Image)
+test_image.landmarks['bb'] = menpo.io.import_landmark_file(
+    '../Dataset/300W/Outdoor/BoundingBoxes/outdoor_004.pts'
+).bounding_box()
+
+test_image_m = mirror_image(test_image)
+assert isinstance(test_image_m, menpo.image.Image)
+
+# Mirror bb
+plt.subplot(211)
+test_image.landmarks['bb_test'] = PointCloud(test_image.landmarks['bb'].points[[0, 1]])
+test_image.view_landmarks(group='bb_test')
+plt.subplot(212)
+test_image_m.landmarks['bb_test'] = PointCloud(test_image_m.landmarks['bb'].points[[0, 1]])
+test_image_m.view_landmarks(group='bb_test')
+plt.show()
+
+# Mirror 68
+plt.subplot(211)
+test_image.landmarks['PTS_test'] = PointCloud(test_image.landmarks['PTS'].points[range(10)])
+test_image.view_landmarks(group='PTS_test')
+plt.subplot(212)
+test_image_m.landmarks['PTS_test'] = PointCloud(test_image_m.landmarks['PTS'].points[range(10)])
+test_image_m.view_landmarks(group='PTS_test')
+plt.show()
+
+# Mirror 73
+test_image = menpo.io.import_image('../Dataset/FW2/Images/t020208.png')
+assert isinstance(test_image, menpo.image.Image)
+test_image.landmarks['bb'] = menpo.io.import_landmark_file(
+    '../Dataset/FW2/Images/t020208.pts'
+).bounding_box()
+
+test_image_m = mirror_image(test_image)
+assert isinstance(test_image_m, menpo.image.Image)
+
+plt.subplot(211)
+test_image.landmarks['PTS_test'] = PointCloud(test_image.landmarks['PTS'].points[range(10)])
+test_image.view_landmarks(group='PTS_test')
+plt.subplot(212)
+test_image_m.landmarks['PTS_test'] = PointCloud(test_image_m.landmarks['PTS'].points[range(10)])
+test_image_m.view_landmarks(group='PTS_test')
+plt.show()
+
+
+# =====Plot test=====
+test_images = menpo.io.import_images('../Dataset/FW2/Images/t02021*.png', verbose=True)
+np_images = [img.pixels.transpose(1, 2, 0) for img in test_images]
+np_lms = [img.landmarks['PTS'].points for img in test_images]
+plot_images = batch_draw_landmarks_discrete(np_images, np_lms, np_lms)
+plot_images = [
+    np.concatenate((plot_images[0], plot_images[1], plot_images[2]), 1),
+    np.concatenate((plot_images[3], plot_images[4], plot_images[5]), 1),
+    np.concatenate((plot_images[6], plot_images[7], plot_images[8]), 1),
+]
+plot_images = np.concatenate((plot_images[0], plot_images[1], plot_images[2]), 0)
+plt.imshow(plot_images)
+plt.show()
--- utils.py
+++ utils.py
@@ -3,74 +3,74 @@ from menpo.shape import PointCloud
 import cv2
 
 # ===== 68 =====
-jaw_indices = np.arange(0, 17)
-lbrow_indices = np.arange(17, 22)
-rbrow_indices = np.arange(22, 27)
-upper_nose_indices = np.arange(27, 31)
-lower_nose_indices = np.arange(31, 36)
-leye_indices = np.arange(36, 42)
-reye_indices = np.arange(42, 48)
-outer_mouth_indices = np.arange(48, 60)
-inner_mouth_indices = np.arange(60, 68)
+_jaw_indices = np.arange(0, 17)
+_left_eyebrow_indices = np.arange(17, 22)
+_right_eyebrow_indices = np.arange(22, 27)
+_upper_nose_indices = np.arange(27, 31)
+_lower_nose_indices = np.arange(31, 36)
+_left_eye_indices = np.arange(36, 42)
+_right_eye_indices = np.arange(42, 48)
+_outer_mouth_indices = np.arange(48, 60)
+_inner_mouth_indices = np.arange(60, 68)
 
-parts_68 = (jaw_indices, lbrow_indices, rbrow_indices, upper_nose_indices,
-            lower_nose_indices, leye_indices, reye_indices,
-            outer_mouth_indices, inner_mouth_indices)
+_parts_68 = (_jaw_indices, _left_eyebrow_indices, _right_eyebrow_indices, _upper_nose_indices,
+             _lower_nose_indices, _left_eye_indices, _right_eye_indices,
+             _outer_mouth_indices, _inner_mouth_indices)
 
-mirrored_parts_68 = np.hstack([
-    jaw_indices[::-1], rbrow_indices[::-1], lbrow_indices[::-1],
-    upper_nose_indices, lower_nose_indices[::-1],
-    np.roll(reye_indices[::-1], 4), np.roll(leye_indices[::-1], 4),
-    np.roll(outer_mouth_indices[::-1], 7),
-    np.roll(inner_mouth_indices[::-1], 5)
+_mirrored_parts_68 = np.hstack([
+    _jaw_indices[::-1], _right_eyebrow_indices[::-1], _left_eyebrow_indices[::-1],
+    _upper_nose_indices, _lower_nose_indices[::-1],
+    np.roll(_right_eye_indices[::-1], 4), np.roll(_left_eye_indices[::-1], 4),
+    np.roll(_outer_mouth_indices[::-1], 7),
+    np.roll(_inner_mouth_indices[::-1], 5)
 ])
 # ===== 68 =====
 
 # ===== 73 =====
-jaw_73 = np.arange(0, 15)
-lbrow_73 = np.arange(15, 21)
-rbrow_73 = np.arange(21, 27)
-leye_73 = np.arange(31, 35)
-reye_73 = np.arange(27, 31)
-nose_73 = np.arange(35, 44)
-nose_hole_73 = np.arange(44, 46)
-nose_point_73 = np.arange(64, 65)
-upper_outer_mouth_73 = np.arange(46, 53)
-lower_outer_mouth_73 = np.arange(53, 58)
-upper_inner_mouth_73 = np.arange(61, 64)
-lower_inner_mouth_73 = np.arange(58, 61)
-eye_ex = np.arange(65, 73)
+_jaw_73 = np.arange(0, 15)
+_left_eyebrow_73 = np.arange(15, 21)
+_right_eyebrow_73 = np.arange(21, 27)
+_left_eye_73 = np.arange(31, 35)
+_right_eye_73 = np.arange(27, 31)
+_nose_73 = np.arange(35, 44)
+_nose_hole_73 = np.arange(44, 46)
+_nose_point_73 = np.arange(64, 65)
+_upper_outer_mouth_73 = np.arange(46, 53)
+_lower_outer_mouth_73 = np.arange(53, 58)
+_upper_inner_mouth_73 = np.arange(61, 64)
+_lower_inner_mouth_73 = np.arange(58, 61)
+_eye_extra = np.arange(65, 73)
 
-parts_73 = (
-    jaw_73,
-    lbrow_73,
-    rbrow_73,
-    reye_73,
-    leye_73,
-    nose_73,
-    nose_hole_73,
-    upper_outer_mouth_73,
-    lower_outer_mouth_73,
-    lower_inner_mouth_73,
-    upper_inner_mouth_73,
-    nose_point_73,
-    eye_ex
+_parts_73 = (
+    _jaw_73,
+    _left_eyebrow_73,
+    _right_eyebrow_73,
+    _right_eye_73,
+    _left_eye_73,
+    _nose_73,
+    _nose_hole_73,
+    _upper_outer_mouth_73,
+    _lower_outer_mouth_73,
+    _lower_inner_mouth_73,
+    _upper_inner_mouth_73,
+    _nose_point_73,
+    _eye_extra
 )
 
-mirrored_parts_73 = np.hstack([
-    jaw_73[::-1],
-    rbrow_73,
-    lbrow_73,
-    leye_73,
-    reye_73,
-    nose_73[::-1],
-    nose_hole_73[::-1],
-    upper_outer_mouth_73[::-1],
-    lower_outer_mouth_73[::-1],
-    lower_inner_mouth_73[::-1],
-    upper_inner_mouth_73[::-1],
-    nose_point_73,
-    eye_ex[::-1]
+_mirrored_parts_73 = np.hstack([
+    _jaw_73[::-1],
+    _right_eyebrow_73,
+    _left_eyebrow_73,
+    _left_eye_73,
+    _right_eye_73,
+    _nose_73[::-1],
+    _nose_hole_73[::-1],
+    _upper_outer_mouth_73[::-1],
+    _lower_outer_mouth_73[::-1],
+    _lower_inner_mouth_73[::-1],
+    _upper_inner_mouth_73[::-1],
+    _nose_point_73,
+    _eye_extra[::-1]
 ])
 # ===== 73 =====
 
@@ -80,16 +80,26 @@ def norm_idx(num_patches):
         return 36, 45
     elif num_patches == 73:
         return 31, 27
-    else:
-        return None, None
+    assert False
+
+
+# =====Mirror=====
+def _mirror_landmarks(landmarks, image_size, mirrored_parts):
+    tmp = np.array([0, image_size[1]]) - landmarks.as_vector().reshape(-1, 2)
+    tmp[:, 0] = -tmp[:, 0]
+    return PointCloud(tmp[mirrored_parts])
 
 
 def mirror_landmarks_68(lms, image_size):
-    return PointCloud(abs(np.array([0, image_size[1]]) - lms.as_vector().reshape(-1, 2))[mirrored_parts_68])
+    return _mirror_landmarks(lms, image_size, _mirrored_parts_68)
 
 
 def mirror_landmarks_73(lms, image_size):
-    return PointCloud(abs(np.array([0, image_size[1]]) - lms.as_vector().reshape(-1, 2))[mirrored_parts_73])
+    return _mirror_landmarks(lms, image_size, _mirrored_parts_73)
+
+
+def mirror_bounding_box(lms, image_size):
+    return _mirror_landmarks(lms, image_size, range(4)).bounding_box()
 
 
 def mirror_image(im):
@@ -97,141 +107,29 @@ def mirror_image(im):
     im.pixels = im.pixels[..., ::-1].copy()
 
     for group in im.landmarks:
-        lms = im.landmarks[group]
-        if lms.points.shape[0] == 68:
-            im.landmarks[group] = mirror_landmarks_68(lms, im.shape)
-        elif lms.points.shape[0] == 73:
-            im.landmarks[group] = mirror_landmarks_73(lms, im.shape)
+        if group == 'PTS':
+            if im.landmarks[group].points.shape[0] == 68:
+                im.landmarks[group] = mirror_landmarks_68(im.landmarks[group], im.shape)
+            elif im.landmarks[group].points.shape[0] == 73:
+                im.landmarks[group] = mirror_landmarks_73(im.landmarks[group], im.shape)
+        elif group == 'bb':
+            im.landmarks[group] = mirror_bounding_box(im.landmarks[group], im.shape)
     return im
 
 
-def mirror_image_bb(im):
-    im = im.copy()
-    im.pixels = im.pixels[..., ::-1]
-    im.landmarks['bounding_box'] = PointCloud(abs(np.array([0, im.shape[
-        1]]) - im.landmarks['bounding_box'].points))
-    return im
+# =====Plot=====
+def draw_landmarks_discrete(image, ground_truth, prediction):
+    image = image.copy()
+    if ground_truth is not None:
+        for v in ground_truth:
+            cv2.circle(image, (int(v[1]), int(v[0])), 1, (1, 0, 0), -1)
+    if prediction is not None:
+        for v in prediction:
+            cv2.circle(image, (int(v[1]), int(v[0])), 1, (1, 1, 1), -1)
+    return image
 
 
-def line(image, x0, y0, x1, y1, color):
-    steep = False
-    if x0 < 0 or x0 >= image.shape[1] or x1 < 0 or x1 >= image.shape[1] or y0 < 0 or y0 >= image.shape[0] or y1 < 0 or y1 >= image.shape[0]:
-        return
-
-    if abs(x0 - x1) < abs(y0 - y1):
-        x0, y0 = y0, x0
-        x1, y1 = y1, x1
-        steep = True
-
-    if x0 > x1:
-        x0, x1 = x1, x0
-        y0, y1 = y1, y0
-
-    for x in range(int(x0), int(x1) + 1):
-        t = (x - x0) / float(x1 - x0)
-        y = y0 * (1 - t) + y1 * t
-        if steep:
-            image[x, int(y)] = color
-        else:
-            image[int(y), x] = color
-
-
-def draw_landmarks(img, lms):
-    try:
-        img = img.copy()
-        if lms.shape[0] == 68:
-            for i, part in enumerate(parts_68[1:]):
-                circular = []
-
-                if i in (4, 5, 6, 7):
-                    circular = [part[0]]
-
-                for p1, p2 in zip(part, list(part[1:]) + circular):
-                    p1, p2 = lms[p1], lms[p2]
-
-                    line(img, p2[1], p2[0], p1[1], p1[0], 1)
-        elif lms.shape[0] == 73:
-            for i, part in enumerate(parts_73[:-2]):
-                circular = []
-                if i in (1, 2, 3, 4):
-                    circular = [part[0]]
-                for p1, p2 in zip(part, list(part[1:]) + circular):
-                    p1, p2 = lms[p1], lms[p2]
-                    line(img, p2[1], p2[0], p1[1], p1[0], 1)
-            # Mouth
-            for p1, p2 in zip(
-                [parts_73[7][-1], parts_73[8][-1], parts_73[7][0], parts_73[7][0], parts_73[7][-1], parts_73[7][-1]],
-                [parts_73[8][0], parts_73[7][0], parts_73[9][0], parts_73[10][-1], parts_73[9][-1], parts_73[10][0]]
-            ):
-                p1, p2 = lms[p1], lms[p2]
-                line(img, p2[1], p2[0], p1[1], p1[0], 1)
-    except BaseException as e:
-        print('[DRAW LMS]', e)
-    return img
-
-
-def batch_draw_landmarks(imgs, lms):
-    return np.array([draw_landmarks(img, l) for img, l in zip(imgs, lms)])
-
-
-def draw_landmarks_discrete(img, gtlms, prlms):
-    img = img.copy()
-    if gtlms is not None:
-        for v in gtlms:
-            cv2.circle(img, (int(v[1]), int(v[0])), 1, (1, 0, 0), -1)
-    if prlms is not None:
-        for v in prlms:
-            cv2.circle(img, (int(v[1]), int(v[0])), 1, (1, 1, 1), -1)
-    return img
-
-
-def batch_draw_landmarks_discrete(imgs, gtlms, prlms):
-    return np.array([draw_landmarks_discrete(img, gtl, prl) for img, gtl, prl in zip(imgs, gtlms, prlms)])
-
-
-def get_central_crop(images, box=(6, 6)):
-    _, w, h, _ = images.get_shape().as_list()
-
-    half_box = (box[0] / 2., box[1] / 2.)
-
-    a = slice(int((w // 2) - half_box[0]), int((w // 2) + half_box[0]))
-    b = slice(int((h // 2) - half_box[1]), int((h // 2) + half_box[1]))
-
-    return images[:, a, b, :]
-
-
-def build_sampling_grid(patch_shape):
-    patch_shape = np.array(patch_shape)
-    patch_half_shape = np.require(np.round(patch_shape / 2), dtype=int)
-    start = -patch_half_shape
-    end = patch_half_shape
-    sampling_grid = np.mgrid[start[0]:end[0], start[1]:end[1]]
-    return sampling_grid.swapaxes(0, 2).swapaxes(0, 1)
-
-
-default_sampling_grid = build_sampling_grid((30, 30))
-
-
-def extract_patches(pixels, centres, sampling_grid=default_sampling_grid):
-    """ Extracts patches from an image.
-
-    Args:
-        pixels: a numpy array of dimensions [width, height, channels]
-        centres: a numpy array of dimensions [num_patches, 2]
-        sampling_grid: (patch_width, patch_height, 2)
-
-    Returns:
-        a numpy array [num_patches, width, height, channels]
-    """
-    pixels = pixels.transpose(2, 0, 1)
-
-    max_x = pixels.shape[-2] - 1
-    max_y = pixels.shape[-1] - 1
-
-    patch_grid = (sampling_grid[None, :, :, :] + centres[:, None, None, :]
-                  ).astype('int32')
-
-    x = patch_grid[:, :, :, 0].clip(0, max_x)
-    y = patch_grid[:, :, :, 1].clip(0, max_y)
-
-    return pixels[:, x, y].transpose(1, 2, 3, 0)
+def batch_draw_landmarks_discrete(images, ground_truths, predictions):
+    return np.array(
+        [draw_landmarks_discrete(img, gt, pr) for img, gt, pr in zip(images, ground_truths, predictions)]
+    )
